298c7fe82c6ce7cb5372c3c138b7880b86f8c47b
goodbye-isaac <goodbyeisaac1727@gmail.com>
2022-09-20 21:11:52 +0900

    add .gitignore

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+  before_action :login_required
+
+  private
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def login_required
+    unless current_user
+      redirect_to login_url
+    end
+  end
 end
